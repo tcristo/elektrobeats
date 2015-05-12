001fd2a8dedc717a70553c7a5960a57b89b8faf5
Thomas Cristo <cristo@strato.de>
2015-05-12 10:21:07 +0200

    add more years

--- src/main/webapp/js/controllers/HomeController.js
+++ src/main/webapp/js/controllers/HomeController.js
@@ -8,6 +8,19 @@ app.controller('HomeController', ['$scope', function($scope) {
         },
         {
             year : 2014
+        },
+        {
+            year : 2013
+        },
+        {
+            year : 2012
+        },
+        {
+            year : 2011
+        },
+        {
+            year : 2010
         }
+
     ]
 }]);
